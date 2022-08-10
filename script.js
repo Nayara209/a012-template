ed9b91e1da5aab6d136df27670679be9e2741e4f
Nayara <nayaraazevwdo@gmail.com>
2022-08-10 14:34:24 -0300

    exercicio-condicionais1

---
+++ script.js
@@ -0,0 +1,38 @@
+const bool1 = false
+const bool2 = true
+const bool3 = true
+
+// if(bool1){
+//     alert("Entrou no if. Bool1 é true")
+// }else{
+//     alert ("Entrou no else. Boll1 é false")
+// }
+
+// if(bool1 === bool2){
+//     alert("o valor de bool1 e bool2 são iguais")
+// }else if(bool2 === bool3){
+//     alert("o valor de bool2 e bool3 são iguais")
+// }else if(bool1 === bool3){
+//     alert("o valor de bool1 e bool3 são iguais")
+// }else{
+//     alert("não existem valores iguais ")
+// }
+
+// let idade = 10
+
+// if(idade >= 60 || idade >= 16 && idade < 18){
+//     console.log("titulo facultativo")
+// }else if(idade >= 18){
+//     console.log("tem direito ao titulo de eleitor")
+// }else{
+//     console.log("vc não tem a idade minima para a emissão do titulo de eleitor")
+// }
+
+const media = 9
+if(media >= 5 ){
+    console.log("Aprovada")
+}else if(media >=3 ){
+    console.log("Recuperaçaõ")
+}else if(media < 3){
+    console.log("Reprovado")
+}else{console.log("Dado invalido")}
